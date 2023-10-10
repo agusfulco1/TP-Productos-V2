24a449151887ab153d81d39dc8327cccdb030c79
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-10 14:30:13 -0300

    casi hecho

--- tp7-productos/src/Components/ProductoCarrito.js
+++ tp7-productos/src/Components/ProductoCarrito.js
@@ -1,7 +1,16 @@
 import { propTypes } from 'react-bootstrap/esm/Image'
+import { useState } from 'react'
 import '../Styles/Carrito.css'
 import PropTypes from 'prop-types'
 export default function ProductoCarrito(ObjetoCarrito) {
+    const [timer, setTimer] = useState()
+
+    const restar = () => {
+        setTimer(timer - 1)
+    }
+    const sumar = () => {
+        setTimer(timer + 1)
+    }
     return (
         <div className="containerCarrito">
             <div className='row'>
@@ -12,6 +21,12 @@ export default function ProductoCarrito(ObjetoCarrito) {
                 </div>
                 <div className='col-md-7 textoCarrito'>
                     <h2>{ObjetoCarrito.ObjetoCarrito.title}</h2>
+                    <div>
+                        <button onClick={sumar()}>+</button>
+                        <h1>{timer}</h1>
+                        <button onClick={restar()}>-</button>
+                    </div>
+                    
                 </div>
                 <div className='col-md-3 textoCarrito'>
                     <h2>{ObjetoCarrito.ObjetoCarrito.price}$</h2>
