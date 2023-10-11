c3d6c9593e3debbaf004ce89028f9074aac20348
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-11 08:55:19 -0300

    falta eliminar carrito

--- tp7-productos/src/Components/ProductoCarrito.js
+++ tp7-productos/src/Components/ProductoCarrito.js
@@ -3,13 +3,22 @@ import { useState } from 'react'
 import '../Styles/Carrito.css'
 import PropTypes from 'prop-types'
 export default function ProductoCarrito(ObjetoCarrito) {
-    const [timer, setTimer] = useState()
+    const [count, setCount] = useState(1)
 
     const restar = () => {
-        setTimer(timer - 1)
+        if (count > 1) {
+            setCount(count => count - 1)
+        }   
     }
+
     const sumar = () => {
-        setTimer(timer + 1)
+        setCount(count => count + 1)
+    }
+
+    const calcularPrecio = () => {
+        let precio = ObjetoCarrito.ObjetoCarrito.price
+        let precioPorCarrito = precio * count
+        return precioPorCarrito
     }
     return (
         <div className="containerCarrito">
@@ -19,17 +28,16 @@ export default function ProductoCarrito(ObjetoCarrito) {
                         <img src={ObjetoCarrito.ObjetoCarrito.thumbnail} className="imagenProducto" alt='no hay imagen'></img>
                     </div>
                 </div>
-                <div className='col-md-7 textoCarrito'>
+                <div className='col-md-6 textoCarrito'>
                     <h2>{ObjetoCarrito.ObjetoCarrito.title}</h2>
-                    <div>
-                        <button onClick={sumar()}>+</button>
-                        <h1>{timer}</h1>
-                        <button onClick={restar()}>-</button>
-                    </div>
-                    
                 </div>
-                <div className='col-md-3 textoCarrito'>
-                    <h2>{ObjetoCarrito.ObjetoCarrito.price}$</h2>
+                <div className='col-md-2 count'>
+                        <button onClick={sumar}>+</button>
+                        <h1>{count}</h1>
+                        <button onClick={restar}>-</button>
+                </div>
+                <div className='col-md-2 textoCarrito'>
+                    <h2>{calcularPrecio()}$</h2>
                 </div>
             </div>
             
